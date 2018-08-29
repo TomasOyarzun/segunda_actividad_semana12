4b411fe0742198ef9c050b32906b110595249870
Tomas <oyarzun.tomas@gmail.com>
2018-08-29 19:54:55 -0300

    ADD: Style to product's CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   root 'pages#home'
-  get 'products/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
